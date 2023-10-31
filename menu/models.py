dd1a4942faa524fa05ac9c40ccafe78157a1ed78
bekzod818 <bekzod030900@gmail.com>
2023-11-01 00:05:20 +0500

    fix: updated

--- menu/models.py
+++ menu/models.py
@@ -15,7 +15,6 @@ class MenuItem(models.Model):
             url_name = named_url_parts[0]
             params = named_url_parts[1:len(named_url_parts)]
             reversed_named_url = reverse(url_name, args=params)
-            print(reversed_named_url)
             if self.explicit_url:
                 if self.explicit_url != reversed_named_url:
                     raise Http404('explicit_url does not match named_url')
